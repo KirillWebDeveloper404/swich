df75f0f06512f42350ce0282099b0e0eebe0c25c
KirillWebDeveloper404 <kirillcernysev148@gmail.com>
2022-05-18 20:43:29 +0300

    1

--- moscow_switch/handlers/users/networking/start.py
+++ moscow_switch/handlers/users/networking/start.py
@@ -59,7 +59,7 @@ async def start(message: Message, state: FSMContext):
                 "Упс... \n""У вас законфилась подписка \n\nПерейдите в раздел тарифы и оплатите подписку")
 
     except Exception as e:
-        print(e)
+        # print(e)
         await message.answer("Упс... \n""У вас законфилась подписка \n\nПерейдите в раздел тарифы и оплатите подписку")
 
 
